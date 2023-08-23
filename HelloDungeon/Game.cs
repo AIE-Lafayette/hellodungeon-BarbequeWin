88083f24d071ed08fb57dfae88d33e150f73edac
KennethJackson <personguy426@gmail.com>
2023-08-23 15:05:58 -0500

    I added in stats and a small introduction to the game.

--- HelloDungeon/Game.cs
+++ HelloDungeon/Game.cs
@@ -10,7 +10,32 @@ namespace HelloDungeon
     {
         public void Run()
         {
+            //The stats of the character.
+            string playerName = "Player";
+            int maxEnemies = 10;
+            bool playerIsAlive = true;
+            float maxPlayerHealth = 100.00f;
+            float maxPlayerStamina = 100.00f;
 
+            //Print out name for first exercise.
+            Console.WriteLine("Kenneth");
+            Console.WriteLine("Jackson");
+
+            playerName = Console.ReadLine();
+
+            Console.WriteLine(playerName);
+
+            //First and last name.
+            string firstname = Console.ReadLine();
+            string lastname = Console.ReadLine();
+            playerName = firstname + lastname;
+
+            //Meeting your first familar face and indroducing yourself.
+            Console.WriteLine("Welcome to the world of Marif!");
+            Console.WriteLine("My name is Willerd Willy, the Sorcerer, but you can just call me Willy.");
+            Console.WriteLine("What is your name?");
+            Console.WriteLine("I see. It's nice to meet you," + playerName + "." );
+            
         }
     }
 }
