4bdc1396f81812202cd978603c1a284c1c4796af
KennethJackson <personguy426@gmail.com>
2023-08-30 16:54:43 -0500

    I added functions and I deleted the loop within a loop.

--- HelloDungeon/Game.cs
+++ HelloDungeon/Game.cs
@@ -8,6 +8,68 @@ namespace HelloDungeon
 {
     class Game
     {
+        float Add(float a, float b)
+        {
+            float result = a + b;
+            return result;
+        }
+
+        int GetGreaterNumber(int a, int b)
+        {
+            if (a > b)
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+        }
+
+        void CountTo(int end)
+        {
+            int count = 0;
+
+            while (count <= end)
+            {
+                Console.WriteLine(count);
+                count++;
+            }
+        }
+
+        string DisplayMenu(string prompt, string option1, string option2, string option3)
+        {
+            string playerChoice = "";
+
+            while (playerChoice != "1" && playerChoice != "2" && playerChoice != "3")
+            {
+                //Display prompt.
+                Console.Clear();
+                Console.WriteLine(prompt);
+                
+                //Display all options.
+                Console.WriteLine("1." + option1);
+                Console.WriteLine("2." + option2);
+                Console.WriteLine("3." + option3);
+
+                //Get player input.
+                Console.Write("> ");
+                playerChoice = Console.ReadLine();
+                
+                //if the player input is not one of the available options...
+                if (playerChoice != "1" && playerChoice != "2" && playerChoice != "3")
+                {
+                    //...display the error message.
+                    Console.Clear();
+                    Console.WriteLine("Invalid Input");
+                    Console.WriteLine("Press any key to continue");
+                    Console.ReadKey(true);
+                }
+            }
+
+            return playerChoice;
+        }
+
         public void Run()
         {
             //The stats of the character.
@@ -15,9 +77,11 @@ namespace HelloDungeon
             string playerChoice = "";
             int areaNumber = 0;
             bool playerIsAlive = true;
+            bool gameOver = false;
             float maxPlayerHealth = 100.00f;
             float maxPlayerStamina = 100.00f;
             float maxPlayerMagic = 100.00f;
+            float playerMoney = 2000.00f;
 
             //Print out name for first exercise.
             Console.WriteLine("Kenneth");
@@ -169,12 +233,39 @@ namespace HelloDungeon
 
             Console.Clear();
 
-            //A loop inside of a loop to let you redecide on your decision.
-            bool decisionMade = false;
+            Console.WriteLine("You realize that you don't have any equipment, so you go to the village for safety.");
+            
+            Console.Write("> ");
 
-            while (decisionMade == false)
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey(true);
+
+            Console.Clear();
+
+            Console.WriteLine("As you walk through the village, you encounter someone on the side of the road who walked up to you to make you buy something.");
+
+            Console.Write("> ");
+
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey(true);
+
+            Console.Clear();
+
+            Console.WriteLine("The person offers you a potion of unknown origin that costs 500 credits.");
+
+            Console.Write("> ");
+
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey(true);
+
+            Console.Clear();
+
+            //Meeting someone trying to offer you a potion.
+            playerChoice = "";
+
+            while (playerChoice != "1")
             {
-                Console.WriteLine("Do you want to go to the village?");
+                Console.WriteLine("You have 2,000 credits. Do you want to buy the potion?");
 
                 Console.WriteLine("1. Yes");
                 Console.WriteLine("2. No");
@@ -183,36 +274,71 @@ namespace HelloDungeon
                 playerChoice = Console.ReadLine();
 
                 Console.Clear();
-
                 if (playerChoice == "1")
                 {
-                    Console.WriteLine("You walk towards the village.");
-
+                    Console.WriteLine("You obtained a health potion.");
                 }
                 else if (playerChoice == "2")
                 {
-
+                    Console.WriteLine("You declined the offer.");
                 }
-
-                playerChoice = "";
-
-                while (playerChoice != "1" && playerChoice != "2")
+                else
                 {
-                    Console.WriteLine("Are you sure?");
-                    Console.WriteLine("1. Yes");
-                    Console.WriteLine("2. No");
-
-                    Console.WriteLine("> ");
-                    playerChoice = Console.ReadLine();
-
-                    if (playerChoice == "1")
-                    {
-                        decisionMade = true;
-                    }
+                    Console.WriteLine("Invalid Input");
                 }
             }
 
+            Console.WriteLine("Before you continue on your way. You should check your stats.");
+
+            Console.Write("> ");
+
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey(true);
+
+            Console.Clear();
+
+            void displayStats(string name, float health, float stamina, float magic, float money)
+            {
+                Console.WriteLine("Name" + name);
+                Console.WriteLine("Health" + health);
+                Console.WriteLine("Stamina" + stamina);
+                Console.WriteLine("Magic" + magic);
+                Console.WriteLine("Money" + money);
+            }
+
             Console.WriteLine("");
+
+
+
+
+
+
+
+
+
+
+            playerChoice = "";
+
+            while (playerChoice != "1" && playerChoice != "2")
+            {
+                Console.WriteLine("Thank you for playing! Do you want to play again?");
+
+                Console.WriteLine("1. Yes");
+                Console.WriteLine("2. No");
+                Console.Write("> ");
+
+                playerChoice= Console.ReadLine();
+
+                if (playerChoice == "2")
+                {
+                    gameOver = true;
+                }
+                else if (playerChoice != "1")
+                {
+                    Console.WriteLine("Invalid Input");
+                }
+                Console.Clear();
+            }
         }
     }
 }
